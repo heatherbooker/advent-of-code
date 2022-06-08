3c260896a1ed2b416302c46fc01038a488531f4f
hboo <heather.booker@hotmail.com>
2022-06-08 16:21:07 -0400

    solve part 2 day 6

--- 2021/06.js
+++ 2021/06.js
@@ -1,16 +1,33 @@
 const input = `3,4,3,1,2`;
 
-const fishes = input.split(',').map(Number);
-
-for (let day = 0; day < 18; day ++) {
-  fishes.forEach((fish, index) => {
-    if (fish === 0) {
-      fishes.push(8);    // creates a new fish
-      fishes[index] = 6; // resets its own timer
+function updateFish(fishByAge) {
+  const newFishes = new Array(9).fill(0);
+  fishByAge.forEach((numOfFish, age) => {
+    if (age === 0) {
+      newFishes[8] = numOfFish; // new baby fish
+      newFishes[6] = numOfFish; // reset their own count
     } else {
-      fishes[index] = fish - 1;
+      newFishes[age - 1] = newFishes[age - 1] + numOfFish;
     }
   });
+  return newFishes;
 }
 
-console.log(fishes.length);
+function inputToFishArray(string) {
+  const fishes = string.split(',').map(Number);
+  let fishByAge = new Array(9).fill(0); // it isn't really age, but close enough
+  fishes.forEach(fish => fishByAge[fish] ++);
+  return fishByAge;
+}
+
+function solve() {
+  let fishByAge = inputToFishArray(input);
+
+  for (let day = 0; day < 256; day ++) {
+    fishByAge = updateFish(fishByAge);
+  }
+  const total = fishByAge.reduce((sum, numOfFishAtAge) => sum + numOfFishAtAge, 0);
+  console.log(total);
+}
+
+solve();
