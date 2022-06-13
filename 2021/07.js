fc4a381497e563ff69a51ae2263ecfb701aa8808
hboo <heather.booker@hotmail.com>
2022-06-13 17:28:03 -0400

    Use dict per age for better performance
    use trick from https://beny23.github.io/posts/advent_of_code_2021_day_7/
    
    speed up to 2.2x user seconds from 3.5x

--- 2021/07.js
+++ 2021/07.js
@@ -2,15 +2,18 @@ const input = `16,1,2,0,4,2,7,1,2,14`;
 
 const crabPositions = input.split(',').map(Number);
 const maxPosition = Math.max(...crabPositions);
+const crabDict = {};
+crabPositions.forEach(crab => crabDict[crab] ? crabDict[crab] ++ : crabDict[crab] = 1);
 
 let minDifference = Infinity;
 
 for (let position = 0; position <= maxPosition; position ++) {
   let totalDifference = 0;
-  crabPositions.forEach(crab => {
-    const diffForCrab = Math.abs(crab - position);
+  Object.keys(crabDict).forEach(crabGroup => {
+    const diffForCrab = Math.abs(crabGroup - position);
+    const numOfCrabs = crabDict[crabGroup];
     for (let i = 0; i <= diffForCrab; i ++) {
-      totalDifference += i;
+      totalDifference += (i * numOfCrabs);
     }
   });
   if (totalDifference < minDifference) {
