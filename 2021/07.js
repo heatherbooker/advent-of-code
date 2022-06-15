30f55f6876b059875d60eb0bb97a563d87fffd36
hboo <heather.booker@hotmail.com>
2022-06-14 22:24:14 -0400

    Make day 7 slightly clearer

--- 2021/07.js
+++ 2021/07.js
@@ -2,20 +2,30 @@ const input = `16,1,2,0,4,2,7,1,2,14`;
 
 const crabPositions = input.split(',').map(Number);
 const maxPosition = Math.max(...crabPositions);
-const crabDict = {};
-crabPositions.forEach(crab => crabDict[crab] ? crabDict[crab] ++ : crabDict[crab] = 1);
+
+function populateCrabDict() {
+  const dict = {};
+  crabPositions.forEach(crab => dict[crab] ? dict[crab] ++ : dict[crab] = 1);
+  return dict;
+}
+
+const crabDict = populateCrabDict();
 
 let minDifference = Infinity;
 
 for (let position = 0; position <= maxPosition; position ++) {
   let totalDifference = 0;
+
   Object.keys(crabDict).forEach(crabGroup => {
     const diffForCrab = Math.abs(crabGroup - position);
     const numOfCrabs = crabDict[crabGroup];
+
     for (let i = 0; i <= diffForCrab; i ++) {
-      totalDifference += (i * numOfCrabs);
+      const fuelUsed = i * numOfCrabs;
+      totalDifference += fuelUsed;
     }
   });
+
   if (totalDifference < minDifference) {
     minDifference = totalDifference;
   }
